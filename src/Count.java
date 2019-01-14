28586cabc39b9bb7bad0845af1d4ee36a1f5b894
Krisztina Nagy <the.nagy.kriszta@gmail.com>
2019-01-14 13:33:00 +0100

    b1.2

--- src/Count.java
+++ src/Count.java
@@ -12,4 +12,11 @@ public class Count {
         System.out.println("https://www.facebook.com/InterestingVideosWorld/posts/548243212308293?__xts__[0]=68.ARAiDy1xZHgoIrRO4-DEpg82IiYk0tYzTlRX_Zy_WuJcoKGuNqk-lg9uR3vAvHTd3Z718cxpx0r-si7DqAkaaGYqmA8tsUJ44ESdkkS9WQrtJ6eQXxKHl7Ziwk9OsmzH6kzH6xAAc8Dzj2LQFZBgqt38Itu3oy4fV3OsbNNbYe9fqz_uJ_RERSH3oIOq_Hu8bnzHl2PzQpdoH2j72M6NA5vszd5tATtjK5cVJCLpRC26NGLyW7kdohItitA5f19wH5k11gnDDqBwx_RGAch4j1mh0XKU65rzbSbkiIu6VfJW6eBl2DxdFFNBN2T-gSosKVXPEz1hpSsXoml6ZigrzWvrxvGQ3FhakuIflQO0NUo70t-dYsrgLxnLVW74-a5f&__tn__=C-R");
     }
 
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
