7815402d2971ec54743750f8bd66ae059efabe35
GreatWolf666 <nagy.kristof94@gmail.com>
2019-01-14 13:46:03 +0100

    parameter 1 added to print

--- src/Greet.java
+++ src/Greet.java
@@ -1,7 +1,8 @@
 public class Greet {
 
-
   public static void main(String[] args) {
     System.out.println("Hello, assholes!");
+    int parameter1 = 15;
+    System.out.println(parameter1);
   }
 }
