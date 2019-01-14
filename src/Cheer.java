b893537a01e26519942d2a8d942f83a549e6c794
Krisztina Nagy <the.nagy.kriszta@gmail.com>
2019-01-14 14:11:41 +0100

    fixed stuff

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,11 +1,11 @@
 public class Cheer {
-<<<<<<< Updated upstream
+
   public void PrintOut(String text) {
-    System.out.println(text);
-=======
+    System.out.println(text); }
+
   public static void main(String[] args) {
     System.out.println("asd");
     System.out.println("Java");
->>>>>>> Stashed changes
+
   }
 }
