faace3099245b0f7cb5bc719250da18d30e5c1fc
Krisztina Nagy <the.nagy.kriszta@gmail.com>
2019-01-14 14:20:23 +0100

    b5

--- src/Count.java
+++ src/Count.java
@@ -4,18 +4,18 @@ public class Count {
 
     public Count(String age){
         this.age = age;
-        this.count(age);
+        this.count(age, "much wow");
     }
 
-    public void count(String age){
-        System.out.println(age);
+    public void count(String age, String other_stuff){
+        System.out.println(age + " " + other_stuff);
     }
 
     public String getAge() {
         return age;
     }
 
-    public void setAge(String age) {
+    public void setAge(String age) {    
         this.age = age;
     }
 }
