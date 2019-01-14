28317de0777a2c78bb8e7095cbb1e41f303be9ba
GreatWolf666 <nagy.kristof94@gmail.com>
2019-01-14 14:13:47 +0100

    fuck this shit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-<<<<<<< Updated upstream
         Count dracula = new Count("42");
 
         Count monthOfBirth = new Count("02");
-=======
         Count dracula   = new Count("42");
->>>>>>> Stashed changes
     }
 }
