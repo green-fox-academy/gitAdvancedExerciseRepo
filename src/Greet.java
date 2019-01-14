434e7c16324549bb3076b47fa345959e0554bdb6
Krisztina Nagy <the.nagy.kriszta@gmail.com>
2019-01-14 14:40:47 +0100

    b6

--- src/Greet.java
+++ src/Greet.java
@@ -3,6 +3,6 @@ public class Greet {
   public static void main(String[] args) {
     System.out.println("Greetings!");
     int parameter1 = 15;
-    System.out.println(parameter1);
+    System.out.println("Greetings to the " + parameter1);
   }
 }
