24f2a8fa5111e4170754079ade12c8441a3f3454
GreatWolf666 <nagy.kristof94@gmail.com>
2019-01-14 14:20:50 +0100

    Two param push

--- src/Greet.java
+++ src/Greet.java
@@ -4,5 +4,11 @@ public class Greet {
     System.out.println("Greetings!");
     int parameter1 = 15;
     System.out.println(parameter1);
+
+    int parameter2 = 25;
+    int parameter3 = 12421;
+
+    System.out.println(parameter2 + parameter3);
+
   }
 }
