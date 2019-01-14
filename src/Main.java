f0df02d15f3d81a1a89818d4b2350bd11f018a72
GreatWolf666 <nagy.kristof94@gmail.com>
2019-01-14 13:53:07 +0100

    birth month printed out

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Count dracula = new Count("42");
+
+        Count monthOfBirth = new Count("02");
     }
 }
